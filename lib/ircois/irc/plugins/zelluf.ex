836f8771c7cf03944b4dc91e56ebfebfeb55ca49
Christophe De Troyer <christophe.detroyer@gmail.com>
2021-04-16 16:01:38 +0200

    regex case sensitivity fix

--- lib/ircois/irc/plugins/zelluf.ex
+++ lib/ircois/irc/plugins/zelluf.ex
@@ -1,7 +1,7 @@
 defmodule Ircois.Plugins.Zelluf do
   use Ircois.Plugin.Macros
 
-  react ~r/.*\sis\s(.+\s)?een(?<wat>(\s*[a-zA-Z]+\s*)+)/, e, probability: 0.05 do
+  react ~r/.*\sis\s(.+\s)?een(?<wat>(\s*[a-zA-Z]+\s*)+)/i, e, probability: 0.05 do
     wat = e.captures["wat"]
 
     w = String.trim(wat)
