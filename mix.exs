a6bca5b9d4ae8df79ee3cc407cf7db0a4c1a1815
Christophe De Troyer <christophe.detroyer@gmail.com>
2021-04-16 15:51:13 +0200

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ircois.MixProject do
   def project do
     [
       app: :ircois,
-      version: "0.3.141592",
+      version: "0.3.1415926",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
